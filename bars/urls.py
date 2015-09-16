97e42054c2cc750a92c4596d63a24790682d6337
rlfrahm <frahmryan@gmail.com>
2015-09-16 12:37:01 -0500

    Fixing error

--- bars/urls.py
+++ bars/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   url(r'^bars/register$', views.registerBarHandler, name='register-bar'),
   url(r'^bars/(?P<bar_id>[0-9]+)$', views.barDetailHandler, name='bar-detail'),
-  url(r'^bars/(?P<bar_id>[0-9]+)/bartenders/invite/(?P<invite_id>.+)$', views.BartenderInviteHandler, name='bartender-invite'),
+  url(r'^bars/(?P<bar_id>[0-9]+)/bartenders/invite/(?P<invite_id>.+)$', views.bartenderInviteHandler, name='bartender-invite'),
 ]
