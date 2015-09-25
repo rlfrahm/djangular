fd5e5aea280024baa4866e246a1920c2bceb0fc9
rlfrahm <frahmryan@gmail.com>
2015-09-25 11:03:25 -0500

    Adding an about page

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^buy-drinks', views.buyDrinksHandler, name='buy_drinks'),
+  # url(r'^buy-drinks', views.buyDrinksHandler, name='buy_drinks'),
+  url(r'^about$', views.aboutHandler, name='about'),
   url(r'^', views.homeHandler, name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.conf import settings
 
+def aboutHandler(request):
+	return render(request, 'about.html')
+
 def homeHandler(request):
   if request.user.is_authenticated() is not True:
     return render(request, 'home-logged-out.html')
