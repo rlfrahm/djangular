bd6f213bba4ac5fe0f53afd6229bcb593ee60dda
rlfrahm <frahmryan@gmail.com>
2015-10-19 15:50:02 -0500

    Removing illegal references

--- bars/models.py
+++ bars/models.py
@@ -1,9 +1,6 @@
-# from django.db import models
+from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.gis.db import models
-from django.contrib.gis.geos import *
-from django.contrib.gis.measure import D
 
 from notifications.emails import send_tab_invite
 
